429c7b1ba9bd9ffb089131d48affe3c446c059ea
Spencer Fink <fink.spencer@gmail.com>
2019-08-14 11:18:43 -0400

    adds images and sets up transitions

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -28,6 +28,8 @@ const ScrollColumn = styled.div`
   max-height: 100vh;
   box-sizing: border-box;
   padding: 10%;
+  opacity: ${props => (props.revealed ? '1' : '0')};
+  transition: opacity 0.5s ease-in-out;
 `;
 
 const BlackSlide = styled.div`
@@ -40,7 +42,14 @@ const BlackSlide = styled.div`
   justify-content: center;
   align-items: center;
   background-color: #000;
+  transform: ${props => (props.revealed ? 'traslateY(0)' : 'translateY(100%)')};
+  transition: transform 1s ease-in-out;
   p.quote {
+    font-size: 32px;
+    color: #fff;
+    width: 50%;
+  }
+  span.credit {
     font-size: 32px;
     color: #fff;
     width: 50%;
@@ -56,6 +65,12 @@ const BlackSlide = styled.div`
 const BigText = styled.h3`
   margin: 5% 0 0 0;
   font-size: 55px;
+  color: ${props => (props.black ? '#000' : '#5a5a5a')};
+`;
+
+const MedText = styled.h3`
+  ${props => (props['margin-top'] ? 'margin-top: auto;' : '')}
+  font-size: 24px;
   color: ${props => (props.black ? '#000' : '#5a5a5a')};
 `;
 
@@ -97,6 +112,7 @@ const Index = () => {
   };
 
   const updateIndex = useCallback(() => {
+    console.log(swiper.realIndex);
     setSwiperIndex(swiper.realIndex + 1);
   }, [swiper]);
 
@@ -121,7 +137,7 @@ const Index = () => {
   return (
     <HorizontalContainer>
       <Swiper {...swiperSettings}>
-        <BlackSlide>
+        <BlackSlide revealed>
           <p className="quote">
             ROW DTLA has shown Los Angeles what happens when you redefine the
             concept of workplace, that if you celebrate innovation,
@@ -130,7 +146,7 @@ const Index = () => {
           </p>
           <span className="scroll-reminder">SCROLL</span>
         </BlackSlide>
-        <ScrollColumn>
+        <ScrollColumn revealed={revealed >= 1}>
           <PhotoRow>
             <ResponsiveImage srcPath="/images/home/row_1" imgAlt="test" />
           </PhotoRow>
@@ -140,7 +156,7 @@ const Index = () => {
             ROW DTLA
           </BigText>
         </ScrollColumn>
-        <ScrollColumn>
+        <ScrollColumn revealed={revealed >= 2}>
           <PhotoRow>
             <ResponsiveImage srcPath="/images/home/row_2" imgAlt="test" />
             <ResponsiveImage srcPath="/images/home/row_3" imgAlt="test" />
@@ -153,7 +169,18 @@ const Index = () => {
             <br />a creative and cultural hub
           </BigText>
         </ScrollColumn>
-        <ScrollColumn>
+        <ScrollColumn revealed={revealed >= 3}>
+          <MedText margin-top>
+            Contemporary offices, bespoke
+            <br /> boutiques, and artisanal eateries
+            <br />
+            catered to the entrepreneurial
+            <br /> visionaries, style icons, and food
+            <br />
+            connoisseurs of Los Angeles
+          </MedText>
+        </ScrollColumn>
+        <ScrollColumn revealed={revealed >= 4}>
           <PhotoRow grid>
             <ResponsiveImage srcPath="/images/home/row_4" imgAlt="test" />
             <ResponsiveImage srcPath="/images/home/row_5" imgAlt="test" />
@@ -163,6 +190,33 @@ const Index = () => {
             <ResponsiveImage srcPath="/images/home/row_7" imgAlt="test" />
           </PhotoRow>
         </ScrollColumn>
+        <ScrollColumn revealed={revealed >= 5}>
+          <ResponsiveImage srcPath="/images/home/row_1" imgAlt="test" />
+        </ScrollColumn>
+        <ScrollColumn revealed={revealed >= 6}>
+          <PhotoRow>
+            <ResponsiveImage srcPath="/images/home/row_8" imgAlt="test" />
+          </PhotoRow>
+          <MedText margin-top>
+            Authentic experiences in a district
+            <br /> charged with innovation, collaboration
+            <br /> and community. Curated down to
+            <br /> every last detail, ROW DTLA's ever-
+            <br />
+            changing environment fosters
+            <br /> creativity in every aspect
+          </MedText>
+        </ScrollColumn>
+        <BlackSlide revealed={revealed >= 7}>
+          <p className="quote">
+            "In Downtown Los Angeles, ROW DTLA has continued to take shape,
+            transforming 32 acres into an all-encompassing iconic destination.
+            It is a growing collective of globally recognized shops, restaurants
+            and innovative offices that are spread over six buildings to create
+            their own unique L.A. environment."
+          </p>
+          <span className="credit">LA MAGAZINE</span>
+        </BlackSlide>
       </Swiper>
     </HorizontalContainer>
   );
