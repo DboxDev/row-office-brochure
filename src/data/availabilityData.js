e1ec90a4dd89d4f7ffd22eaa29e31c5b836f7733
Joel Hoelting <joelhoelting@protonmail.com>
2019-10-28 12:28:34 -0400

    Adds mock data object for new availability data

--- src/data/availabilityData.js
+++ src/data/availabilityData.js
@@ -1,3 +1,63 @@
+// const availabilityData = [
+//   {
+//     sqFt: '3,500-7,000',
+//     addresses: [
+//       {
+//         title: '757 S. ALAMEDA',
+//         floorplans: []
+//       },
+//       {
+//         title: '767 S. ALAMEDA',
+//         floorplans: [
+//           { number: 410, address: 767 },
+//           { number: 418, address: 767 },
+//           { number: 434, address: 767 },
+//           { number: 465, address: 767 }
+//         ]
+//       }
+//     ]
+//   },
+//   {
+//     sqFt: '7,000-15,000',
+//     addresses: [
+//       {
+//         title: '757 S. ALAMEDA',
+//         floorplans: []
+//       },
+//       {
+//         title: '767 S. ALAMEDA',
+//         floorplans: [{ number: 400, address: 767 }]
+//       }
+//     ]
+//   },
+//   {
+//     sqFt: '15,000-35,000',
+//     addresses: [
+//       {
+//         title: '757 S. ALAMEDA',
+//         floorplans: []
+//       },
+//       {
+//         title: '767 S. ALAMEDA',
+//         floorplans: []
+//       }
+//     ]
+//   },
+//   {
+//     sqFt: '35,000-200,000',
+//     addresses: [
+//       {
+//         title: '757 S. ALAMEDA',
+//         floorplans: [{ number: 300, address: 757 }]
+//       },
+//       {
+//         title: '767 S. ALAMEDA',
+//         floorplans: []
+//       }
+//     ]
+//   }
+// ];
+
 const availabilityData = [
   {
     address: '767 S. ALAMEDA',
